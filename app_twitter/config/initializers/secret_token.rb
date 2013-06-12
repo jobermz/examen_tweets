b346314ae69fea22abcb0cac16637bf6dd0bacee
UPC_SEDE_SAN_BORJA <spantigo@upc.edu.pe>
2013-06-11 20:34:23 -0500

    primera version

---
+++ app_twitter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppTwitter::Application.config.secret_token = '964cb4fb8130bed25f5ebadb496db98589920dc87356ca9f8853023b2e9537c0a6d27b66038f23e29f6d80365b808b8e4587e9c460d521bd8153c601c78236e2'
